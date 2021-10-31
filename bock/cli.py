3ea07d4e405fe359a0b3743adf8a120f1b763ad4
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-31 16:53:36 +0100

    #9. created very basic cli setup

--- bock/cli.py
+++ bock/cli.py
@@ -1,29 +1,27 @@
 import os
-
 import click
 
+# commands to be loaded for click
+# according to: https://click.palletsprojects.com/en/8.0.x/commands/#nested-handling-and-contexts
 
 class ComplexCLI(click.MultiCommand):
 
-  def test():
-    print("bla")
+    def list_commands(self, ctx):
+        commands = []
+        commands_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
+        for filename in os.listdir(commands_folder):
+            if filename.endswith(".py") and filename != '__init__.py':
+                commands.append(filename.replace(".py", ""))
 
-    # def list_commands(self, ctx):
-    #     commands = []
-    #     commands_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
-    #     for filename in os.listdir(commands_folder):
-    #         if filename.endswith(".py") and filename.startswith("cmd_"):
-    #             commands.append(filename.replace("cmd_", "").replace(".py", ""))
+        commands.sort()
+        return commands
 
-    #     commands.sort()
-    #     return commands
-
-    # def get_command(self, ctx, name):
-    #     try:
-    #         mod = __import__(f"eve.commands.cmd_{name}", None, None, ["cli"])
-    #     except ImportError:
-    #         return
-    #     return mod.cli
+    def get_command(self, ctx, name):
+        try:
+            mod = __import__(f"bock.commands.{name}", None, None, ["cli"])
+        except ImportError:
+            return
+        return mod.cli
 
 
 
---
+++ bock/commands/demo.py
@@ -0,0 +1,29 @@
+
+import click
+
+# Base Setup
+#
+# class definition is needed for external data and basic setup here
+class Context:
+    def __init__(self):
+        # self.note = svc_note.Note()
+        pass
+
+# assign defined context to command initialization.
+@click.group()
+@click.pass_context
+def cli(ctx):
+    """Demo command template for building bock commands"""
+    ctx.obj = Context()
+    pass
+
+# 
+#
+#
+
+# first real command in subcommand
+@cli.command()
+@click.pass_context
+def testme(ctx):
+  """ Test out bock funtionality"""
+  click.echo("This is a test!")
