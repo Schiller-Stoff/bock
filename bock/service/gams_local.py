7a76e05a3e56d68b899a4f49dcd410545a2ddbd0
Sebastian Stoff <sebastianstoff@gmx.at>
2021-10-31 17:48:49 +0100

    #3. rooted gams-local verification stronger inside class

--- bock/service/gams_local.py
+++ bock/service/gams_local.py
@@ -4,13 +4,20 @@ from pathlib import Path
 
 class GamsLocal:
   def __init__(self) -> None:
-      self.home_path = str(Path.home())
       self.default_gamslocal = "gams-local"
-      pass
+      self.home_path = str(Path.home())
+      self.gamslocal_path = self.get_gamslocal_root()
 
-  def verify_rootfolder(self) -> bool:
+  def verify_rootfolder(self, exp_gamslocal_path: Path) -> bool:
     """
     Method checks if the default wsl root-folder is correctly setup.
     """
-    exp_gamslocal_path = Path( self.home_path + os.sep + self.default_gamslocal )
     return Path.exists(exp_gamslocal_path) and Path.is_dir(exp_gamslocal_path)
+
+  def get_gamslocal_root(self) -> Path:
+    """
+    Retrieves Path representation of gams-local folder.
+    """
+    exp_gamslocal_path = Path( self.home_path + os.sep + self.default_gamslocal )
+    self.verify_rootfolder(exp_gamslocal_path=exp_gamslocal_path)
+    return exp_gamslocal_path
