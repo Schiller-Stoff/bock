ea168c1c3fbf994f5c0e65d5a1ad24c14ed85216
Sebastian Stoff <sebastianstoff@gmx.at>
2021-11-01 14:38:55 +0100

    #10. on instantation gams-local will check if wsl is available

--- bock/service/gams_local.py
+++ bock/service/gams_local.py
@@ -1,13 +1,29 @@
 
 import os
 from pathlib import Path
+from bock.utils.wsl import WSL
 
 class GamsLocal:
   def __init__(self) -> None:
-      self.default_gamslocal = "gams-local"
-      self.home_path = str(Path.home())
-      self.gamslocal_path = self.get_gamslocal_root()
+    self.check_wsl_active()
+    self.default_gamslocal = "gams-local"
+    # self.home_path = str(Path.home())
+    # self.gamslocal_path = self.get_gamslocal_root()
 
+  def check_wsl_active(self) -> True:
+    """
+    Returns True if wsl is the executing context. Otherwise will throw a NotImplementedError
+    """
+     # check if wsl is the execution environment of the script  
+    if WSL.in_wsl(): 
+      return True
+    else:
+      # check if wsl would be available  
+      if WSL.win_wsl_available():
+        raise NotImplementedError("Running bock ouside wsl is currently not supported, but WSL seems to be installed. Type 'wsl' in your cmd and start bock from there!")
+      else:
+        raise NotImplementedError("Running bock ouside wsl is currently not supported. WSL seems not to be installed")
+  
   def verify_rootfolder(self, exp_gamslocal_path: Path) -> bool:
     """
     Method checks if the default wsl root-folder is correctly setup.
