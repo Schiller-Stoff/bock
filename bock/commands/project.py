0b7a551e895be8a8278ec7ed02ddb1c202aed9ed
Sebastian Stoff <sebastianstoff@gmx.at>
2021-11-01 14:39:15 +0100

    #10. moved impl to constructor of gamsLocal

--- bock/commands/project.py
+++ bock/commands/project.py
@@ -28,20 +28,6 @@ def cli(ctx):
 def setup(ctx):
   """ Setup a gams4+ project for your local gams."""
 
-  # check if wsl is the execution environment of the script  
-  if WSL.in_wsl(): 
-    click.echo("WSL is the executing environment!")
-    # do further stuff here
-    # default_root = ctx.obj.gams_local.get_gamslocal_root()
-    # print(str(default_root))
-
-  else:
-    click.echo("**NOT SUPPORTED: Running bock outside WSL.**")
-    # check if wsl would be available  
-    if WSL.win_wsl_available():
-      click.echo("WSL seems to be installed. Type 'wsl' in your cmd to start and start bock from there!")
-      pass
-    else:
-      click.echo("WSL not installed!")
-    return
-  
+  click.echo("NICE")
+  # default_root = ctx.obj.gams_local.get_gamslocal_root()
+  # print(str(default_root))
