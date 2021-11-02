bfcfd527c54d7a66f6016fffdf138dfbf7a4265d
Sebi <SebastianStoff@gmx.at>
2021-11-02 14:31:59 +0100

    #10. added small test code

--- bock/commands/demo.py
+++ bock/commands/demo.py
@@ -1,6 +1,8 @@
 
 import click
 
+from bock.utils.wsl import WSL
+
 # Base Setup
 #
 # class definition is needed for external data and basic setup here
@@ -26,4 +28,5 @@ def cli(ctx):
 @click.pass_context
 def testme(ctx):
   """ Test out bock funtionality"""
-  click.echo("This is a test!")
+  click.echo("This is a test!, Ubuntu LTS 20 being used? ")
+  click.echo(WSL.check_for_ubuntu20_lts())
