7c4c8f57f636efe37a4c1b593a912c19e45c7fc3
Sebi <SebastianStoff@gmx.at>
2021-11-02 14:02:35 +0100

    #10. added info command to retrieve info about the system in use

---
+++ bock/commands/info.py
@@ -0,0 +1,37 @@
+
+import click
+import platform
+
+# Base Setup
+#
+# class definition is needed for external data and basic setup here
+class Context:
+    def __init__(self):
+        # self.note = svc_note.Note()
+        pass
+
+# assign defined context to command initialization.
+@click.group()
+@click.pass_context
+def cli(ctx):
+    """Providing info about your system especially in hinsight to your wsl installation"""
+    ctx.obj = Context()
+    pass
+
+# 
+#
+#
+
+# first real command in subcommand
+@cli.command()
+@click.pass_context
+def system(ctx):
+  """
+  Provides 
+  """
+  click.echo(f"Running system: {platform.system()}")
+  click.echo(f"OS-release: {platform.release()}")
+  click.echo(f"Uname: {platform.uname()}")
+  click.echo(f"Machine type: {platform.machine()}")
+  click.echo(f"Network-node: {platform.node()}")
+  click.echo(f"Uing python version: {platform.python_version()}")
