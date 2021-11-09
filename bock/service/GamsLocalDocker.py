8aff51cb68fe3ecd7409701be6f9dc51249f8de6
Sebi <SebastianStoff@gmx.at>
2021-11-09 14:18:06 +0100

    #22. Added method that grabs all logs and copies it to gams-logs

---
+++ bock/service/GamsLocalDocker.py
@@ -0,0 +1,54 @@
+
+
+import os
+import tempfile
+import subprocess
+from shutil import copytree
+
+class GamsLocalDocker:
+  """
+
+  """
+
+  DOCKER_CONTAINER = ["activemq", "blazegraph", "cocoon", "fcrepo4", "latex", "nexuscube", "proai", "relight", "skosifier", "toolbox", "triplestore",
+                      "apache", "cm4f", "fcgate", "fits", "loris", "postgres", "rdf4j", "rscript", "solr", "treetagger", "verovio"]
+
+  def __init__(self) -> None:
+      pass
+
+
+  @staticmethod
+  def grab_logging(location: str) -> str:
+    """
+    Copies all available gams-local logs to specified location into gams-logs folder.
+    :param location target to where logs should be copied. 
+    :return path to the logs.
+    """
+
+    gams_logspath = location + os.sep + "gams-logs"
+    if not os.path.isdir(gams_logspath):
+      os.mkdir(gams_logspath)
+
+    with tempfile.TemporaryDirectory() as tmpdirpath:
+      
+      logs_temp = tmpdirpath + os.sep + "logs"
+      os.mkdir(logs_temp)  
+
+      for container in GamsLocalDocker.DOCKER_CONTAINER:
+        cmd = f"docker logs {container}"
+        ret = subprocess.check_output(cmd, shell=True, text=True).strip().replace("  ", " ")
+
+        f = open( logs_temp + os.sep + f"{container}.log", "a")
+        f.write(ret)
+        f.close()
+
+        copytree(logs_temp, gams_logspath, dirs_exist_ok=True)
+
+    print("*** \n")
+    print("\n")
+    print("\n")
+    print(f"Copied all gams-local logs to: {gams_logspath}")
+
+    return gams_logspath
+
+      
